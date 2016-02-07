30b3b054ea51dd78ca6281561cea4952c94ded10
imashkan <ashkanpj.81@gmail.com>
2016-02-07 23:27:04 +0330

    a

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -6,7 +6,7 @@ local bot_id = our_id -- your bot id
     if matches[1] == 'leave' and is_admin(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-       send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups.', ok_cb, false)
+       send_large_msg("chat#id"..msg.to.id, 'متاسفیم این گروه معلق به فایر نیست و فرد اد کننده بات به گروه بلاک میشود تا دیگر نتواند اد کند.', ok_cb, false)
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     end
 end
