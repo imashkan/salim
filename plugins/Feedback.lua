2cf12fab85661f5b5ab2045e7ebda9348d92177f
imashkan <ashkanpj.81@gmail.com>
2016-02-08 17:00:15 +0330

    a

--- plugins/Feedback.lua
+++ plugins/Feedback.lua
@@ -19,7 +19,7 @@ local fuses = '!printf user#id' .. msg.from.id
  else
 
 
-                 local sends0 = send_msg('chat#107855523', fuse, ok_cb, false)
+                 local sends0 = send_msg('101854916', fuse, ok_cb, false)
 
  return 'پیام شما با موفقیت به تیم قدرتمند فایر فرستاده شد.'
 
